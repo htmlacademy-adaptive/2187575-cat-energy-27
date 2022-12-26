1e0ef05eae4ca7dab35bd92271af55ce2dd05f3e
Keks <keks@htmlacademy.ru>
2022-12-27 00:02:19 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/menu.js
@@ -0,0 +1,14 @@
+let navToggle = document.querySelector('.main-header__nav-toggle');
+let siteList = document.querySelector('.site-list');
+
+siteList.classList.add('site-list--close');
+
+navToggle.onclick = function () {
+  if (siteList.classList.contains('site-list--close')){
+    siteList.classList.remove('site-list--close');
+  } else {
+    siteList.classList.add('site-list--close');
+  }
+ navToggle.classList.toggle('nav-toogle-close');
+ navToggle.classList.toggle('nav-toogle-open');
+};
